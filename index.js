d025283d5c37dbe2c06a6c7d6154cf33de3f6f52
Nabi-Yi <nabiyi926@gmail.com>
2023-01-10 20:55:41 +0900

    implement get, post, delete data

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const express = require("express");
 const app = express();
+const {PrismaClient} = require("@prisma/client")
 
-app.get("/", (reg, res) => {
-    return res.send("hello world");
+const prisma = new PrismaClient();
+app.use(express.json());
+app.get("/", async (reg, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        return res.status(200).json({users});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
-app.listen(3000,() => {
+app.post("/", async (req, res) => {
+    try {
+        const newUser = await prisma.user.create({
+            data:{
+                nickname: req.body.nickname,
+                email:req.body.email,
+                password: req.body.password,
+                provider: req.body.provider,
+                agree: (req.body.agree === "false" || req.body.agree === "0") ? false : true,
+            },
+        });
+        return res.status(201).json({newUser});
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+app.delete("/", async (req, res) => {
+    try{
+        const deletedUser =await prisma.user.delete({
+            where:{
+                user_id: Number(req.body.user_id),
+            }
+        })
+        return res.status(200).json(deletedUser);
+    }catch (error){
+        console.log(error);
+        const message = error.meta;
+        return res.status(500).json({error: message})
+    }
+})
+app.listen(3000, () => {
     console.log("server on 3000");
 });
